0fc088229807cf542f8e8e28a38b0e5d387e0e06
VitaliyNosov <webdevelorestudio2017@gmail.com>
2023-06-02 18:54:42 +0300

    work

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,28 @@
 console.log('test');
+
+// select block
+
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdown = document.querySelector('.dropdown');
+    var select = dropdown.querySelector('select');
+    var button = dropdown.querySelector('.button');
+  
+    select.addEventListener('click', function() {
+      dropdown.classList.toggle('open');
+    });
+});
+
+
+function toggleElement() {
+    var element = document.getElementById("myElement");
+    var button = document.getElementById("myButton");
+
+    if (element.style.display === "none") {
+    element.style.display = "block";
+    button.classList.add("black-button");
+    } else {
+    element.style.display = "none";
+    button.classList.remove("black-button");
+   }
+}
+
