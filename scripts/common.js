3e93736bb4ebd16e6d0250b6bf574e6aa1acb83c
VitaliyNosov <webdevelorestudio2017@gmail.com>
2023-06-04 19:25:18 +0300

    commit

--- scripts/common.js
+++ scripts/common.js
@@ -70,5 +70,92 @@ function incrementCounter() {
 }
 
 
+// slider one
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  focusOnSelect: true
+});
+
+$('a[data-slide]').click(function(e) {
+  e.preventDefault();
+  var slideno = $(this).data('slide');
+  $('.slider-nav').slick('slickGoTo', slideno - 1);
+});
 
 
+// Если у вас больше одного блока с фото, вам может потребоваться добавить идентификаторы или классы для точного выбора элементов.
+// В этом примере предполагается, что у вас только один блок с фото и видео.
+
+// const photoBlock = document.querySelector('.photo-block');
+// const video = photoBlock.querySelector('video');
+
+// photoBlock.addEventListener('mouseover', function() {
+//   video.play();
+// });
+
+// photoBlock.addEventListener('mouseout', function() {
+//   video.pause();
+//   video.currentTime = 0;
+// });
+
+
+
+
+const photoBlock = document.querySelector('.photo-block');
+const video = photoBlock.querySelector('video');
+
+function playVideo() {
+  return new Promise((resolve, reject) => {
+    if (video.paused) {
+      const playPromise = video.play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => resolve())
+          .catch(error => reject(error));
+      }
+    } else {
+      resolve();
+    }
+  });
+}
+
+function pauseVideo() {
+  return new Promise((resolve, reject) => {
+    if (!video.paused) {
+      video.pause();
+      video.currentTime = 0;
+    }
+    resolve();
+  });
+}
+
+photoBlock.addEventListener('mouseenter', function() {
+  playVideo()
+    .then(() => {
+      // Video started playing successfully
+      console.log('Video started playing');
+    })
+    .catch(error => {
+      // Error occurred while starting video playback
+      console.error('Failed to start video playback:', error);
+    });
+});
+
+photoBlock.addEventListener('mouseleave', function() {
+  pauseVideo()
+    .then(() => {
+      // Video paused and reset successfully
+      console.log('Video paused and reset');
+    });
+});
