95ebee9f6fc1ec1172ceb6a247f22aeb6f418570
VitaliyNosov <webdevelorestudio2017@gmail.com>
2023-06-04 19:32:05 +0300

    commit

--- scripts/common.js
+++ scripts/common.js
@@ -70,28 +70,28 @@ function incrementCounter() {
 }
 
 
-// slider one
+// // slider one
 
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: false,
-  focusOnSelect: true
-});
+// $('.slider-for').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   asNavFor: '.slider-for',
+//   dots: false,
+//   focusOnSelect: true
+// });
 
-$('a[data-slide]').click(function(e) {
-  e.preventDefault();
-  var slideno = $(this).data('slide');
-  $('.slider-nav').slick('slickGoTo', slideno - 1);
-});
+// $('a[data-slide]').click(function(e) {
+//   e.preventDefault();
+//   var slideno = $(this).data('slide');
+//   $('.slider-nav').slick('slickGoTo', slideno - 1);
+// });
 
 
 // Если у вас больше одного блока с фото, вам может потребоваться добавить идентификаторы или классы для точного выбора элементов.
